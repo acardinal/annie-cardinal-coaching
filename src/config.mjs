7e442e3ae84b2ed41391fdae848a7f83b4fb37de
Glenn R. Fisher <glennrfisher@gmail.com>
2023-05-12 18:08:47 -0500

    Update config.mjs

--- src/config.mjs
+++ src/config.mjs
@@ -1,13 +1,13 @@
 import defaultImage from './assets/images/default.png';
 
 const CONFIG = {
-  name: 'AstroWind',
+  name: 'Annie Cardinal Coaching',
 
-  origin: 'https://astrowind.vercel.app',
+  origin: 'https://anniecardinalcoaching.com',
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'AstroWind — Free template for create a website with Astro + Tailwind CSS',
+  title: 'Annie Cardinal Coaching',
   description:
     '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
   defaultImage: defaultImage,
